11929baefa9c090dd8093d3c002396108cf6863b
JiecaoBoy <wxdyl0915@gmail.com>
2023-02-16 23:06:33 -0500

    test

---
+++ check_surface.cs
@@ -0,0 +1,22 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using System.IO;
+using System.Linq;
+
+public class check_surface: MonoBehaviour
+{
+    // Different from check_circumsphere, this program should be able to take in multiple surface vertices and triangles
+    // the goal is to check if the vertex lies in a set of surface triangles
+    public void in_surface(){
+        //First regenterate the five points we have 
+
+
+        Debug.Log("I am here!");
+    }
+
+
+
+}
